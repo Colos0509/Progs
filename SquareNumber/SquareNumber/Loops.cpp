699baf1c664b9ebfc65e518f8b3571d9d8539bb6
Gergo Varga <varga.gergo0509@gmail.com>
2020-11-04 17:30:02 +0000

    loops

--- SquareNumber/SquareNumber/Loops.cpp
+++ SquareNumber/SquareNumber/Loops.cpp
@@ -7,19 +7,58 @@ int main()
 {
 	int numbers[TOTAL] = { 78, 42, 7, 8, 15, 2, 83, 64, 23, 13 };
 
+	//Challenge #1
+
 	for (int i = 0; i < TOTAL; i++)
 	{
 		if (numbers[i] % 2 == 0)
 		{
 			std::cout << "The number" << numbers[i] << "is even" << std::endl;
 		}
-	
+
 		else
 		{
 			std::cout << "The number" << numbers[i] << " is odd" << std::endl;
 		}
 	}
 
+	std::cout << "===========================================================================================" << std::endl;
+
+	//Challenge #2
+
+	int i = 0;
+	while (i < TOTAL)
+
+		if (numbers[i] % 2 == 0)
+		{
+			std::cout << "The number" << numbers[i] << "is even" << std::endl;
+		}
+		
+		else
+		{
+			std::cout << "The number" << numbers[i] << "is odd" << std::endl;
+		}
+
+	i++;
+
+
+	std::cout << "===========================================================================================" << std::endl;
+
+	//challenge #3
+
+	for (int i : numbers)
+	{
+		if (i % 2 == 0)
+		{
+			std::cout << "The number" << i << "is even" << std::endl;
+		}
+
+		else
+		{
+			std::cout << "The number" << i << "is odd" << std::endl;
+		}
+	}
 	system("pause");
 	return 0;
 }
+
